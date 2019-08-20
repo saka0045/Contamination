24a9057476de7904bf8fe9a9446a721b303ad328
m006703 <sakai.yuta@mayo.edu>
2019-08-20 15:11:33 -0500

    Added seqtk bash script

---
+++ scripts/seqtk.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+##################################################
+#Global Variables
+##################################################
+
+INPUT_FASTQ=""
+OUT_FASTQ=""
+SEED=""
+READS=""
+CMD=""
+
+##################################################
+#FUNCTIONS
+##################################################
+
+function usage(){
+cat << EOF
+concatenates fastq files from all lanes, but separate out by
+different reads
+
+OPTIONS:
+    -h  [optional] help, show this message
+    -i  [required] full path to input fastq file
+    -o  [required] full path to output fastq file
+    -s  [required] random seed number
+    -r  [required] number of reads to subset to
+
+EOF
+}
+
+##################################################
+#BEGIN PROCESSING
+##################################################
+
+while getopts "hi:o:s:r:" OPTION
+do
+    case $OPTION in
+		h) usage ; exit ;;
+		i) INPUT_FASTQ=${OPTARG} ;;
+		o) OUT_FASTQ=${OPTARG} ;;
+		s) SEED=${OPTARG} ;;
+		r) READS=${OPTARG} ;;
+    esac
+done
+
+CMD="/usr/local/biotools/seqtk/1.3-r106/seqtk sample -s ${SEED} ${INPUT_FASTQ} ${READS} > ${OUT_FASTQ}"
+echo "Executing command ${CMD}"
+${CMD}
